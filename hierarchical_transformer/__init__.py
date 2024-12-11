0d39bf1f42fa0affafde7c6d761594f2428d6920
jthompson-messina <jthompson@messinagroupinc.com>
2024-12-11 08:25:18 -0600

    Update __init__.py

--- hierarchical_transformer/__init__.py
+++ hierarchical_transformer/__init__.py
@@ -1,1 +1,12 @@
+# hierarchical_transformer/__init__.py
 
+from .processor import DataTransformer, TransformerConfig
+from .validation import DataValidator
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "DataTransformer",
+    "TransformerConfig",
+    "DataValidator"
+]
